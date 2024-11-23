e9f5085eb23a1080401d688968f9a120b1ce8536
Jel1ySpot <Jel1ySpot@outlook.com>
2024-11-24 02:05:48 +0800

    Releast v0.0.1

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
+> This project is in early developing. We need your help to make it perfect
 # conic
+___
+> Another easy way for configuration
+
+## Install
+```shell
+go get -u github.com/Jel1ySpot/conic
+```
+
+## How conic works?
+By binding variable and configuration, you can access and change the configuration in a easy way.
+> Configuration (usually a file) <=> Go Variable
+
+## Getting start
+### Quick start
+```go
+package main
+
+import (
+    "fmt"
+    "github.com/Jel1ySpot/conic"
+)
+
+type Config struct {
+    Name string `json:"name"`
+    Age  int    `json:"age"`
+}
+
+func main() {
+    config := Config{}
+    
+    // Binding
+    conic.BindRef("", &config)
+
+    // Read configuration file
+    conic.SetConfigFile("config.json")
+    conic.ReadInConfig()
+    conic.WatchConfig()
+    /* config.json
+       {
+         "name": "alex",
+         "age": 21
+       }
+    */
+
+    fmt.Printf("&v", config)
+    
+    // Change value
+    config.Name = "Richard"
+    config.Age += 7
+    conic.WriteConfig()
+    /* config.json
+    {
+      "name": "alex",
+      "age": 28
+    }
+    */
+}
+```
--- conic.go
+++ conic.go
@@ -4,11 +4,9 @@ import (
     "fmt"
     "github.com/Jel1ySpot/conic/internal/adapter"
     jsonAdapter "github.com/Jel1ySpot/conic/internal/adapter/json"
-    "github.com/fsnotify/fsnotify"
     "os"
     "path/filepath"
     "strings"
-    "sync"
 )
 
 // UnsupportedConfigError denotes encountering an unsupported
@@ -108,44 +106,6 @@ func New() *Conic {
     return c
 }
 
-// ReadInConfig loads the configuration file
-func ReadInConfig() error { return c.ReadInConfig() }
-
-func (c *Conic) ReadInConfig() error {
-    c.logger("attempting to read in config file")
-    if c.configFile == "" {
-        return NoConfigFileError{}
-    }
-
-    if err := c.setConfigAdapter(); err != nil {
-        return err
-    }
-
-    file, err := os.ReadFile(c.configFile)
-    if err != nil {
-        return ConfigFileReadError{err}
-    }
-
-    var config map[string]any
-
-    err = c.adapter.Decode(file, &config)
-    if config == nil || err != nil {
-        return ConfigFileReadError{err}
-    }
-
-    c.config = config
-
-    defer func() {
-        if len(c.onConfigLoad) > 0 {
-            for _, f := range c.onConfigLoad {
-                go f()
-            }
-        }
-    }()
-
-    return unmarshalAll()
-}
-
 func SetLogger(logger func(format string, args ...interface{})) {
     c.SetLogger(logger)
 }
@@ -198,57 +158,6 @@ func (c *Conic) getConfigType() string {
     }
 
     return ""
-}
-
-// WatchConfig starts watching a config file for changes.
-func WatchConfig() { c.WatchConfig() }
-
-// WatchConfig starts watching a config file for changes.
-func (c *Conic) WatchConfig() {
-    initWG := sync.WaitGroup{}
-    initWG.Add(1)
-    go func() {
-        watcher, err := fsnotify.NewWatcher()
-        if err != nil {
-            c.logger(fmt.Sprintf("failed to create watcher: %s", err))
-            os.Exit(1)
-        }
-        defer watcher.Close()
-
-        eventsWG := sync.WaitGroup{}
-        eventsWG.Add(1)
-        go func() {
-            for {
-                select {
-                case event, ok := <-watcher.Events:
-                    if !ok { // 'Events' channel is closed
-                        eventsWG.Done()
-                        return
-                    }
-
-                    if event.Has(fsnotify.Write) || event.Has(fsnotify.Create) {
-                        if err := c.ReadInConfig(); err != nil {
-                            c.logger(fmt.Sprintf("read config file: %s", err))
-                        }
-                    } else if event.Has(fsnotify.Remove) {
-                        eventsWG.Done()
-                        return
-                    }
-
-                case err, ok := <-watcher.Errors:
-                    if ok { // 'Errors' channel is not closed
-                        c.logger(fmt.Sprintf("watcher error: %s", err))
-                    }
-                    eventsWG.Done()
-                    return
-                }
-            }
-        }()
-        watcher.Add(c.configFile)
-        initWG.Done()   // done initializing the watch in this go routine, so the parent routine can move on...
-        eventsWG.Wait() // now, wait for event loop to end in this go-routine...
-    }()
-    initWG.Wait() // make sure that the go routine above fully ended before returning
 }
 
 func searchMap(source map[string]any, path []string) map[string]any {
