feba9242669cbc8139f4e6686225a2f9aa588fd1
Jel1ySpot <Jel1ySpot@outlook.com>
2024-11-23 14:27:24 +0800

    WriteConfig()

--- file.go
+++ file.go
@@ -1,5 +1,22 @@
 package conic
 
-var (
-    configFilePath string
-)
+import "os"
+
+func WriteConfig() error { return c.WriteConfig() }
+
+func (c *Conic) WriteConfig() error {
+    if err := c.marshalAll(); err != nil {
+        return err
+    }
+
+    b, err := c.adapter.Encode(c.config)
+    if err != nil {
+        return err
+    }
+
+    if err := os.WriteFile(c.configFile, b, os.ModePerm); err != nil {
+        return err
+    }
+
+    return nil
+}
--- go.mod
+++ go.mod
@@ -4,8 +4,6 @@ go 1.20
 
 toolchain go1.23.2
 
-require (
-	github.com/fsnotify/fsnotify v1.8.0 // indirect
-	github.com/spf13/cast v1.7.0 // indirect
-	golang.org/x/sys v0.27.0 // indirect
-)
+require github.com/fsnotify/fsnotify v1.8.0
+
+require golang.org/x/sys v0.27.0 // indirect
